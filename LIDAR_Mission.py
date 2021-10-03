251adb6e13eb9270f90a585ff19eded497f2ef08
Drakonkinst <drakonkinstmc@gmail.com>
2021-10-03 15:33:07 -0700

    Start on mission replay script

--- LIDAR_Mission.py
+++ LIDAR_Mission.py
@@ -34,10 +34,11 @@ from bosdyn.client.world_object import WorldObjectClient
 
 class MissionInterface:
     # INIT
-    def __init__(self, robot, upload_input, download_output):
+    def __init__(self, robot, upload_input, mission_file, download_output):
         self._robot = robot
         self._upload_input = upload_input
         self._download_output = download_output
+        self._mission_file = mission_file
         
         self._waypoint_id = 'NONE'
         self._waypoint_list = []
@@ -53,6 +54,13 @@ class MissionInterface:
         self._init_clients()
     
     def _init_clients(self):
+        
+        # TODO: Difference between mission file and map directory? do we need both?
+        
+        if not os.path.isdir(self._upload_input):
+            LOGGER.error("Cannot find mission file: " + self._upload_input)
+            sys.exit(1)
+        
         self._lease_client = self._robot.ensure_client(LeaseClient.default_service_name)
         try:
             self._estop_client = self._robot.ensure_client(EstopClient.default_service_name)
@@ -68,11 +76,13 @@ class MissionInterface:
         
         self._recording_client = self._robot.ensure_client(GraphNavRecordingServiceClient.default_service_name)
         self._graph_nav_client = self._robot.ensure_client(GraphNavClient.default_service_name)
+        self._mission_client = robot.ensure_client(bosdyn.mission.client.MissionClient.default_service_name)
     
     def __del__(self):
         self.shutdown()
     
     # MISSION
+    
     def start(self):
         self._lease = self._lease_client.acquire()
         if self._lease is None:
@@ -83,34 +93,140 @@ class MissionInterface:
         if self._estop_endpoint is not None:
             self._estop_endpoint.force_simple_setup()
         
-        # This clears the graph!!
-        self._graph_nav_client.clear_graph()
-    
     def run_mission(self):
         with LeaseKeepAlive(self._lease_client):
             # Start
             self._estop_alive()
             self._power_on()
-            self._self_right()
-            self._sit()
-            self._stand()
             
+            # Load input graph
+            self._upload_graph()
+            self._upload_mission()
+            
+            LOGGER.info("Commanding robot to stand...")
+            self._self_right()
+            self._countdown(5)
+            self._sit()
+            self._countdown(5)
+            self._stand()
+            self._countdown(5)
+            LOGGER.info("Robot standing...")
+            
+            # This clears the graph!!
+            self._graph_nav_client.clear_graph()
+        
             # Start recording
             self._start_recording()
             
-            # Do mission
+            # # Localize robot
+            # localization_error = False
+            # if do_localization:
+            #     graph = graph_nav_client.download_graph()
+            #     robot.logger.info('Localizing robot...')
+            #     robot_state = robot_state_client.get_robot_state()
+            #     localization = nav_pb2.Localization()
+
+            #     # Attempt to localize using any visible fiducial
+            #     graph_nav_client.set_localization(
+            #         initial_guess_localization=localization, ko_tform_body=None, max_distance=None,
+            #         max_yaw=None,
+            #         fiducial_init=graph_nav_pb2.SetLocalizationRequest.FIDUCIAL_INIT_NEAREST)
             
+            # # Do mission
+            # if not localization_error:
+            #     self._run_mission(robot, mission_client, lease_client, fail_on_question, args.timeout)
+                
+            # Wait till mission is done?
             
             # Stop recording
             self._stop_recording()
             
-            # Stop
             self._sit()
+            
+            # Download output graph
+            self._download_output()
+            
+            # Stop
             self._safe_power_off()
             self._estop_kill()
             self.shutdown()
             
+    # UPLOAD
+    
+    def _upload_graph(self):
+        if not os.path.isdir(self._upload_input):
+            LOGGER.error("Cannot find mission file: " + self._upload_input)
+            sys.exit(1)
+        
+        graph_filename = os.path.join(self._upload_input, "graph")
+        LOGGER.info("Loading graph from " + graph_filename)
+        
+        with open(graph_filename, "rb") as graph_file:
+            data = graph_file.read()
+            current_graph = map_pb2.Graph()
+            current_graph.ParseFromString(data)
+            LOGGER.info("Loaded graph has {} waypoints and {} edges".format(
+                len(current_graph.waypoints), len(current_graph.edges)))
+            
+        # Load waypoint snapshots
+        current_waypoint_snapshots = dict()
+        for waypoint in current_graph.waypoints:
+            snapshot_filename = os.path.join(path, "waypoint_snapshots", waypoint.snapshot_id)
+            LOGGER.info("Loading waypoint snapshot from " + snapshot_filename)
+            
+            with open(snapshot_filename, "rb") as snapshot_file:
+                waypoint_snapshot = map_pb2.WaypointSnapshot()
+                waypoint_snapshot.ParseFromString(snapshot_file.read())
+                current_waypoint_snapshots[waypoint_snapshot.id] = waypoint_snapshot
+        
+        # Load edge snapshots
+        current_edge_snapshots = dict()
+        for edge in current_graph.edges:
+            snapshot_filename = os.path.join(path, "edge_snapshots", edge.snapshot_id)
+            LOGGER.info("Loading edge snapshot from " + snapshot_filename)
+            
+            with open(snapshot_filename, "rb") as snapshot_file:
+                edge_snapshot = map_pb2.EdgeSnapshot()
+                edge_snapshot.ParseFromString(snapshot_file.read())
+                current_edge_snapshots[edge_snapshot.id] = edge_snapshot
+        
+        # Upload graph to robot
+        LOGGER.info("Uploading graph and snapshots to robot...")
+        self._graph_nav_client.upload_graph(graph=current_graph, lease=self._lease)
+        LOGGER.info("Uploaded graph.")
+        
+        # Upload snapshots
+        for snapshot_id in response.unknown_waypoint_snapshot_ids:
+            waypoint_snapshot = current_waypoint_snapshots[snapshot_id]
+            self._graph_nav_client.upload_waypoint_snapshot(waypoint_snapshot=waypoint_snapshot, lease=lease)
+            LOGGER.info('Uploaded {}'.format(waypoint_snapshot.id))
+
+        for snapshot_id in response.unknown_edge_snapshot_ids:
+            edge_snapshot = current_edge_snapshots[snapshot_id]
+            self._graph_nav_client.upload_edge_snapshot(edge_snapshot=edge_snapshot, lease=lease)
+            LOGGER.info('Uploaded {}'.format(edge_snapshot.id))
+    
+    def _upload_mission(self):
+        LOGGER.info("Loading mission from " + self._mission_file)
+        
+        with open(self._mission_file, "rb") as mission_file:
+            data = mission_file.read()
+            mission_proto = nodes_pd2.Node()
+            mission_proto.ParseFromString(data)
+        
+        # Upload mission
+        LOGGER.info("Uploading mission to robot...")
+        self._mission_client.load_mission(mission_proto, leases=[self._lease])
+        LOGGER.info("Uploaded mission.")
+
     # ACTIONS
+    
+    # Pause for the given duration IN SECONDS
+    def _countdown(self, duration):
+        for i in range(duration, 0, -1):
+            print(i, end=' ', flush=True)
+            time.sleep(1)
+        print(0)
     
     def _power_on(self):
         # Turn the robot on
@@ -209,6 +325,9 @@ class MissionInterface:
         self._waypoint_commands += [self._waypoint_id]
         self._waypoint_commands += ['LOCALIZE']
         return True
+        
+    def _replay_mission(self):
+        pass
     
     # HELPERS
     
@@ -393,7 +512,9 @@ def parse_arguments(argv):
     bosdyn.client.util.add_common_arguments(parser)
     
     # Can add more command line arguments here
-    parser.add_argument("directory", help="Output directory for graph map and mission file.")
+    parser.add_argument("--output", help="Output directory for graph map and mission file.")
+    parser.add_argument('--input', help='Input path to map directory'
+    parser.add_argument('--mission', dest='mission_file', help='Optional path to mission file, defaults to autogenerated one inside input directory')
     
     return parser.parse_args(argv)
     
@@ -425,17 +546,24 @@ def init_robot(hostname, username, password):
     return robot
 
 def main(argv):
-    config = parse_arguments(argv)
+    options = parse_arguments(argv)
     stream_handler = setup_logging(options.verbose)
     robot = None
     
+    if not options.input or not options.output:
+        raise Exception("Must specify input and output paths")
+        
+    if not options.mission_file:
+        options.mission_file = os.path.join(options.input, "missions", "autogenerated")
+        
+    
     try:
-        robot = init_robot(config.hostname, config.username, config.password)
+        robot = init_robot(options.hostname, options.username, options.password)
     except RpcError as e:
         LOGGER.error("Failed to communicate with robot: %s" % e)
         return False
     
-    mission_interface = MissionInterface(robot, options.input, options.output)
+    mission_interface = MissionInterface(robot, options.input, options.mission_file, options.output)
     try:
         mission_interface.start()
     except Exception as e:
